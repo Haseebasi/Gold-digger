c4a9ff8f47368fd499ca9f9b607753b9de014e7c
Haseebasi <haseebasi005@gmail.com>
2026-07-23 11:20:38 +0530

    added transaction updation functionality to server

--- handlers/routehandlers.js
+++ handlers/routehandlers.js
@@ -1,10 +1,28 @@
 import path from 'node:path'
 import fs from 'node:fs/promises'
+import { getData } from '../utils/getData'
 
 export function handlePost(req,res){
-     const data = parseJSONBody(req)
-     
+    try{
+     const parsedData = parseJSONBody(req)
+     await addTransaction(data)
+     sendResponse(res, 201, 'application/json', JSON.stringify(parsedData))
+    }catch(err){
+        sendResponse(res, 400, 'application/json', JSON.stringify({error: err}))
+    }
 }
-function addTransaction(newTransaction){
+async function addTransaction(newTransaction){
+    try { 
 
+    const transactions = await getData()
+    transactions.push(newTransaction)
+    const pathJSON = path.join('data', 'data.json')
+    await fs.writeFile(
+      pathJSON,
+      JSON.stringify(transactions, null, 2),
+      'utf8'
+    )
+  } catch (err) {
+    throw new Error(err)
+  }
 }
